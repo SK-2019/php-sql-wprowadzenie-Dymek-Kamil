b3970eeffe03f6863694d4f644f9d43d310f16da
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2020-11-19 11:56:27 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,6 +4,6 @@ $sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
 echo($sql);
 mysqli_query($conn,$sql);
 mysqli_close($conn);
-header("location:http://127.0.0.1/cos/formularz.php");
+header("location:https://testing-some-stuff.herokuapp.com/formularz.php");
 
 ?>
