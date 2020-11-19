33d6941d026e4b7d7b4ec0bfcfb9a36da493157f
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2020-11-19 11:12:16 +0100

    Add files via upload

--- strona.php
+++ strona.php
@@ -4,14 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
+    <div class="con">
+        <div class="item b">
+            <h1>Dodano:</h1>
 <?php
-        echo("To jest strona.php");
         echo("<li> Imie:".$_POST['imie']."</li>");
         echo("<li> Dzial:".$_POST['dzial']."</li>");
         echo("<li> Zarobki:".$_POST['zarobki']."</li>");
         echo("<li> Data:".$_POST['data_']."</li>");
     ?>
+    </div>
+    </div>
 </body>
 </html>
