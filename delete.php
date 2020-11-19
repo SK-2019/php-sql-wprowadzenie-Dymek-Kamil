975356166c9fc87fc0fc69b6f0cc0539d3d28e60
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2020-11-19 11:01:16 +0100

    Delete delete.php

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("connect.php");
-$sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
-echo($sql);
-mysqli_query($conn,$sql);
-mysqli_close($conn);
-header("location:https://testing-some-stuff.herokuapp.com/")
-?>
