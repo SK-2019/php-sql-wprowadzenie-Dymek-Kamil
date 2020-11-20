bcee85973a597cbe86dbcd71598f014447a3927e
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2020-11-20 14:28:46 +0100

    Add files via upload

--- strona.php
+++ strona.php
@@ -15,10 +15,7 @@
         echo("Dzial:".$_POST['dzial']."<br>");
         echo("Zarobki:".$_POST['zarobki']."<br>");
         echo("Data:".$_POST['data_']); 
-      require_once("connect.php");
-      $sql =  "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (NULL,'".$_POST['imie']."','".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_']."')";
-      mysqli_query($conn,$sql);
-      mysqli_close($conn);
+
     ?>
     <a class="k" href="index.php">Wróć do strony głównej</a>
     </div>
