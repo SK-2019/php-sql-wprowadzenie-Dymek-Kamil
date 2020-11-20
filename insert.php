6ef1ff956e9b6712d3f99a1d1354956b84b96597
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2020-11-20 15:29:32 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,8 +1,4 @@
 <?php
-  echo("<li> Imie: ".$_POST['Imie']."</li>");
-  echo("<li> Dzial: ".$_POST['dzial']."</li>");
-  echo("<li> Zarobki: ".$_POST['zarobki']."</li>");
-  echo("<li> Data: ".$_POST['data_']."</li>");
 require_once("connect.php");
 $sql =  "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (NULL,'".$_POST['imie']."','".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_']."')";
 echo($sql);
