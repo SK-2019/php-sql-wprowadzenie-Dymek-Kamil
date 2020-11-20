4cc560064890e106fb6dcffd21a6284e64df223d
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2020-11-20 15:28:27 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,5 +4,5 @@ $sql = "DELETE FROM pracownicy WHERE id_pracownicy='".$_POST['id']."'";
 echo($sql);
 mysqli_query($conn,$sql);
 mysqli_close($conn);
-header("location:http://127.0.0.1/cos/insert/danedobazy.html");
+header("location:https://testing-some-stuff.herokuapp.com/daneDoBazy.html");
 ?>
