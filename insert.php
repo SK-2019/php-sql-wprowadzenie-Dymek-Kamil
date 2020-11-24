0c72d9a9e8b694550d043570e282f72e95bcce5f
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2020-11-24 09:48:46 +0100

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,9 +1,16 @@
 <?php
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 require_once("connect.php");
 $sql =  "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (NULL,'".$_POST['imie']."','".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_']."')";
 echo($sql);
-mysqli_query($conn,$sql);
-mysqli_close($conn);
-header("location:https://testing-some-stuff.herokuapp.com/daneDoBazy.html");
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+header("location:http://127.0.0.1/cos/insert/danedobazy.html");
 ?>
 
