616313b67b1bb7ef5949ad26c7c23aeb83700f8a
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2021-01-26 09:09:35 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $conn= new mysqli("mysql-muffie.alwaysdata.net","muffie","kamilbaza123","muffie_db");
-    if ($conn->connect_error) {
-        die("connection failed: ".mysqli_connect_error());
-    }
-?>
