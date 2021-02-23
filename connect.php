be6b55f9637d4fe9c2be67f97735578f0b47a05d
Dymek Kamil <kamil.dymek33@gmail.com>
2021-02-23 10:12:20 +0100

    a

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
+//require_once("config.php");
     $conn= new mysqli($_SERVER['addres'],$_SERVER['username'],$_SERVER['pass'],$_SERVER['db']);
     if ($conn->connect_error) {
         die("connection failed: ".mysqli_connect_error());
