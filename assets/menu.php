aed84f0f7d6bf98ba97f48e166c2b9bbac569acb
Dymek-Kamil <73522899+Dymek-Kamil@users.noreply.github.com>
2021-04-22 11:10:10 +0200

    Add files via upload

---
+++ assets/menu.php
@@ -0,0 +1,28 @@
+            <h2>Menu</h2>
+            <a href="../index.php" class="sg" id="sg">Strona Główna</a>
+            <div class="menuprac" id="menuprac">Pracownicy</div>
+                <div id="menup" class="menup">
+                    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Dymek-Kamil">GITHUB</a>
+                    <a href="/../Pracownicy/Pracownicy.php">Pracownicy</a>
+                    <a href="/../Pracownicy/Pracownicy i Organizacja.php">Pracownicy i Organizacja</a>
+                    <a href="/../Pracownicy/Funkcje agregujące.php">Funkcje Agregujące</a>
+                    <a href="/../Pracownicy/data i czas.php">Data i Czas</a>
+                </div>
+            <div class="menufor" id="menufor">Formularze</div>
+                <div id="menuf" class="menuf">
+                    <a href="../Formularze/formularz.html">Formularz</a>
+                    <a href="../Formularze/daneDoBazy.php">Danedobazy</a>
+                </div>
+            <div id="menubib" class="menubib">Biblioteka</div>
+                <div id="menub" class="menub">
+                    <a href="../Biblioteka/książki.php">Książki</a>
+                    <a href="../Biblioteka/wypożyczalnia.php">Wypożyczalnia</a>
+                </div> 
+                <div class="menucw">Cwiczenia</div>
+                    <div class="menuc">
+                <a href="../Cwiczenia/index.html">Flexbox</a>
+                <a href="../Cwiczenia/Biblioteka.php">Biblioteka</a>
+                <a href="../Cwiczenia/Szkola.php">Szkoła</a> 
+                <a href="../Cwiczenia/Przychodznia.php">Przychodnia</a> 
+                <a href="../Cwiczenia/Serwis Samochodowy">Serwis Samochodowy</a>  
+                    </div>  
--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var dropdown = document.getElementsByClassName("menuprac");
     for (i = 0; i < dropdown.length; i++) {
     dropdown[i].addEventListener("click", function() {
     this.classList.toggle("activesidebar");
-    var dropdownContent = this.nextElementSibling;
+    var dropdownContent = document.getElementsByClassName("menup");
     if (dropdownContent.style.display === "block") {
     dropdownContent.style.display = "none";
     } else {
@@ -19,7 +19,7 @@ var dropdown = document.getElementsByClassName("menuprac");
     for (i = 0; i < dropdownfor.length; i++) {
     dropdownfor[i].addEventListener("click", function() {
     this.classList.toggle("activesidebar");
-    var dropdownContentfor = this.nextElementSibling;
+    var dropdownContentfor = document.getElementsByClassName("menuf");
     if (dropdownContentfor.style.display === "block") {
     dropdownContentfor.style.display = "none";
     } else {
@@ -33,7 +33,7 @@ var dropdown = document.getElementsByClassName("menuprac");
     for (i = 0; i < dropdownbib.length; i++) {
     dropdownbib[i].addEventListener("click", function() {
     this.classList.toggle("activesidebar");
-    var dropdownContentbib = this.nextElementSibling;
+    var dropdownContentbib = document.getElementsByClassName("menu");
     if (dropdownContentbib.style.display === "block") {
     dropdownContentbib.style.display = "none";
     } else {
@@ -41,43 +41,14 @@ var dropdown = document.getElementsByClassName("menuprac");
     }
     });
  }
- var dropdownpios = document.getElementsByClassName("piosenki");
-    var i;
 
-    for (i = 0; i < dropdownpios.length; i++) {
-    dropdownpios[i].addEventListener("click", function() {
-    var dropdownContentpios = this.nextElementSibling;
-    if (dropdownContentpios.style.display === "block") {
-    dropdownContentpios.style.display = "none";
-    } else {
-    dropdownContentpios.style.display = "block";
-    }
-    });
- }
  function openNav() {
-    document.getElementById("mySidenav").style.left = "0px";
-    document.getElementById("myCon").style.marginleft = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.left = "-250px";
-    document.getElementById("myCon").style.marginleft = "0px";
-    document.getElementById("MenuP").style.display = "none";
-    document.getElementById("menuf").style.display = "none";
-    document.getElementById("menub").style.display = "none";
-}
-var dropdownA = document.getElementsByClassName("opcja 1");
-    var i;
-
-    for (i = 0; i < dropdownA.length; i++) {
-    dropdownA[i].addEventListener("click", function() {
-    this.classList.toggle("activeopcje");
-    var dropdownContentA = document.getElementById("alliwant");
-    if (dropdownContentA.style.display === "block") {
-    dropdownContentA.style.display = "none";
-    } else {
-    dropdownContentA.style.display = "block";
+    if(document.getElementById("mySidenav").style.left === "0px"){
+        document.getElementById("mySidenav").style.left = "-250px";
     }
-    });
- }
+    else{
+        document.getElementById("mySidenav").style.left = "0px";
+    }
 
+}
 
